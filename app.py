e40fff7fce2f0bca5e970eeb2443dee29b7fef59
Suman Gangopadhyay <linuxgurusuman@gmail.com>
2023-07-28 11:49:53 +0530

    full ml code

--- app.py
+++ app.py
@@ -4,22 +4,30 @@
 from flask import Flask
 from flask import request
 from flask import render_template
+import pickle
+import numpy as np
 
 # create an object of the class Flask
 app = Flask(__name__)
 
+# read the model from the pickle file
+model = pickle.load(open('suman_test_model.pkl', 'rb'))
+
 # URL for home-page/index web page
 @app.route("/")
 def index():
-    return "This is the index page for the Flask and ML application"
+    return render_template('index.html')
 
-@app.route("/suman")
-def suman():
-    return "This is the web-page for Suman Gangopadhyay !"
 
-@app.route("/guvi")
-def guvi():
-    return "This is the webpage for Guvi !"
+@app.route('/result', methods=['POST'])
+def result():
+    # extract all the information from the HTML form
+    blood_sugar_features = [int(x) for x in request.form.values()]
+    print(blood_sugar_features)
+    final_result = [np.array(blood_sugar_features)]
+    prediction = model.predict(final_result)
+    output = round(prediction[0], 2)
+    return render_template('index.html', output=output)
 
 
 # Flask server
