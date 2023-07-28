4b9d57ae433b0a964b98f01269217f36d8628096
Suman Gangopadhyay <linuxgurusuman@gmail.com>
2023-07-28 11:04:45 +0530

    app.py v1

--- app.py
+++ app.py
@@ -1,2 +1,28 @@
+# app.py
 # Main Flask file
 
+from flask import Flask
+from flask import request
+from flask import render_template
+
+# create an object of the class Flask
+app = Flask(__name__)
+
+# URL for home-page/index web page
+@app.route("/")
+def index():
+    return "This is the index page for the Flask and ML application"
+
+@app.route("/suman")
+def suman():
+    return "This is the web-page for Suman Gangopadhyay !"
+
+@app.route("/guvi")
+def guvi():
+    return "This is the webpage for Guvi !"
+
+
+# Flask server
+
+if __name__ == "__main__":
+    app.run(port=5000, debug=True)
